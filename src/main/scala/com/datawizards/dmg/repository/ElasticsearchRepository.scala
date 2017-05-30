e4a0c97d3813ed9605b5ac415fef6167a6131ed0
kalan <piotr.kalanski@gmail.com>
2017-05-30 20:59:23 +0200

    delete es index resolves #56

--- src/main/scala/com/datawizards/dmg/repository/ElasticsearchRepository.scala
+++ src/main/scala/com/datawizards/dmg/repository/ElasticsearchRepository.scala
@@ -3,6 +3,10 @@ package com.datawizards.dmg.repository
 trait ElasticsearchRepository {
   def updateTemplate(templateName: String, mapping: String): Unit
   def getTemplate(templateName: String): String
+  def deleteTemplate(templateName: String): Unit
+  def templateExists(templateName: String): Boolean
   def createIndex(indexName: String, mapping: String): Unit
   def getIndexSettings(indexName: String): String
+  def deleteIndex(indexName: String): Unit
+  def indexExists(indexName: String): Boolean
 }
--- src/main/scala/com/datawizards/dmg/service/ElasticsearchService.scala
+++ src/main/scala/com/datawizards/dmg/service/ElasticsearchService.scala
@@ -13,15 +13,33 @@ trait ElasticsearchService {
 
   def updateTemplate[T: ClassTag: TypeTag](templateName: String): Unit = {
     val template = DataModelGenerator.generate[T](dialects.Elasticsearch)
+    repository.deleteTemplate(templateName)
     repository.updateTemplate(templateName, template)
     log.info(s"Updated template [$templateName] at Elasticsearch.")
+  }
+
+  def updateTemplateIfNotExists[T: ClassTag: TypeTag](templateName: String): Unit = {
+    val template = DataModelGenerator.generate[T](dialects.Elasticsearch)
+    if(!repository.templateExists(templateName)) {
+      repository.updateTemplate(templateName, template)
+      log.info(s"Updated template [$templateName] at Elasticsearch.")
+    }
   }
 
   def getTemplate(templateName: String): String =
     repository.getTemplate(templateName)
 
+  def createIndexIfNotExists[T: ClassTag: TypeTag](indexName: String): Unit = {
+    val mapping = DataModelGenerator.generate[T](dialects.Elasticsearch)
+    if(!repository.indexExists(indexName)) {
+      repository.createIndex(indexName, mapping)
+      log.info(s"Created index [$indexName] at Elasticsearch.")
+    }
+  }
+
   def createIndex[T: ClassTag: TypeTag](indexName: String): Unit = {
     val mapping = DataModelGenerator.generate[T](dialects.Elasticsearch)
+    repository.deleteIndex(indexName)
     repository.createIndex(indexName, mapping)
     log.info(s"Created index [$indexName] at Elasticsearch.")
   }
